9cdceddc01af98ad9447dcb700504120799dcee7
mathnebula186f <getgopalbansal@gmail.com>
2024-02-14 19:51:57 +0530

    cors policy again changed

--- api/index.js
+++ api/index.js
@@ -27,7 +27,11 @@ const bcryptSalt=bcrypt.genSaltSync(10);
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    credentials :true,
+    origin:process.env.CLIENT_URL,
+
+}));
 
 app.get('/test', (req,res) => {
     res.json('test ok');
